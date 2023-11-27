4131d2eb927289f3fbaaad3628320dc292d97ad7
rutvi04 <151378325+resonateworkshop@users.noreply.github.com>
2023-11-26 22:59:14 -0500

    update

--- app.py
+++ app.py
@@ -291,21 +291,21 @@ with st.container():
 
             with col3:
                 image_path = base64.b64encode(open(r"/mount/src/resonate/Images/leadership_logo-removebg-preview.png", 'rb').read()).decode()
-                content = sum_behaviour_made.index[0].split('-')[1]
+                content = sum_behaviour_made.index[0].split('-', 1)[1]
                 col3.markdown(card_style_desc(image_path, content), unsafe_allow_html=True)
             with col4:
                 col4.markdown(card_style_value(str(int(sum_behaviour_made.values[0]))), unsafe_allow_html=True)
 
             with col3:
                 image_path = base64.b64encode(open(r"/mount/src/resonate/Images/business_logo-removebg-preview.png", 'rb').read()).decode()
-                content = sum_behaviour_made.index[1].split('-')[1]
+                content = sum_behaviour_made.index[1].split('-', 1)[1]
                 col3.markdown(card_style_desc(image_path, content), unsafe_allow_html=True)
             with col4:
                 col4.markdown(card_style_value(str(int(sum_behaviour_made.values[1]))), unsafe_allow_html=True)
 
             with col3:
                 image_path = base64.b64encode(open(r"/mount/src/resonate/Images/new_job_logo-removebg-preview.png", 'rb').read()).decode()
-                content = sum_behaviour_made.index[2].split('-')[1]
+                content = sum_behaviour_made.index[2].split('-', 1)[1]
                 col3.markdown(card_style_desc(image_path, content), unsafe_allow_html=True)
             with col4:
                 col4.markdown(card_style_value(str(int(sum_behaviour_made.values[2]))), unsafe_allow_html=True)
