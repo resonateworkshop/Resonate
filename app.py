eb28e1c8e8a84efe847095e0ca9901db377b6fbe
rutvi04 <97nisargpatel@gmail.com>
2024-02-14 21:01:02 -0500

    update

--- app.py
+++ app.py
@@ -80,7 +80,6 @@ behaviour_filter = st.sidebar.multiselect('Select Behaviour', options=behaviour_
 
 year_filtered_data = raw[(raw['Year'].isin(year_filter))]
 year_filtered_data['Year'] = year_filtered_data['Year'].astype(int  )
-st.write(year_filtered_data)
 
 filtered_data = raw[(raw['Age_bins'].isin(age_filter))]
 impact_data = raw[raw.columns[raw.columns.isin(impact_columns)].values]
@@ -91,6 +90,7 @@ behaviour_filtered_data = raw[raw.columns[raw.columns.isin(behaviour_filter)].va
 filtered_data = filtered_data.merge(year_filtered_data, left_index=True, right_index=True,how = 'inner', suffixes=('_x', None))
 filtered_data = filtered_data.merge(impact_filtered_data, left_index=True, right_index=True,how = 'inner', suffixes=('_x', None))
 filtered_data = filtered_data.merge(behaviour_filtered_data, left_index=True, right_index=True,how = 'inner', suffixes=('_x', None))
+st.write(year_filtered_data)
 
 result = filtered_data.groupby(['Year', 'Quarter', 'Age_bins'])['Participant ID'].count().reset_index()
 ### sunburst plot - Age bins
