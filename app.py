3882966cf1e3d86428aa3f03e05f8b7b5f0a040c
rutvi04 <97nisargpatel@gmail.com>
2024-02-14 21:52:25 -0500

    update

--- app.py
+++ app.py
@@ -123,6 +123,7 @@ for index, row in result_melted.groupby('Impact')['perc_impact'].sum().reset_ind
 ### line plot - behaviour
 result = filtered_data.groupby(['Year', 'Quarter'])[behaviour_filter].sum().reset_index()
 result_melted = pd.melt(result, id_vars=['Year','Quarter'], var_name='Behaviour', value_name='Sum')
+st.write(result_melted)
 fig_beh = px.line(result_melted, x=['Quarter'], y='Sum', color='Behaviour', markers = True, 
                   title='Total participants for each quarter by Behaviour', color_discrete_sequence=custom_palette,
                   labels={
